b42d64a0bf45e2e1f9dc5e6704b02299b8de5de5
gits <gits@evangelion.snotfcp.com>
2022-07-25 09:04:17 +0100

    Adapt some code from ACI to ACR

--- acrlabs_scripts/acr-flp-labs.sh
+++ acrlabs_scripts/acr-flp-labs.sh
@@ -2,7 +2,7 @@
 
 ## script name: acr-flp-labs.sh
 ## Version v0.0.1 20220725
-## Set of tools to deploy ACI troubleshooting labs
+## Set of tools to deploy ACR Troubleshooting Labs
 
 ## "-l|--lab" Lab scenario to deploy
 ## "-r|--region" region to deploy the resources
@@ -11,7 +11,7 @@
 ## "--version" print version
 
 ## read the options
-TEMP=`getopt -o g:n:l:r:u:hv --long resource-group:,name:,lab:,region:,user:,help,validate,version -n 'aci-flp-labs.sh' -- "$@"`
+TEMP=`getopt -o g:n:l:r:u:hv --long resource-group:,name:,lab:,region:,user:,help,validate,version -n 'acr-flp-labs.sh' -- "$@"`
 eval set -- "$TEMP"
 
 ## set an initial value for the flags
@@ -87,7 +87,7 @@ function check_resourcegroup_cluster () {
         echo -e "\nResource group $RESOURCE_GROUP already exists...\n"
     fi
 
-    ACI_EXIST=$(az container show -g $RESOURCE_GROUP -n $ACI_NAME &>/dev/null; echo $?)
+    ACR_EXIST=$(az acr show -g $RESOURCE_GROUP -n $ACR_NAME &>/dev/null; echo $?)
     if [ $ACI_EXIST -eq 0 ]
     then
         echo -e "\n--> Container Registry $ACR_NAME already exists...\n"
@@ -99,42 +99,44 @@ function check_resourcegroup_cluster () {
 ## Validate ACR exists
 function validate_acr_exists () {
     RESOURCE_GROUP="$1"
-    ACI_NAME="$2"
+    ACR_NAME="$2"
 
-    ACI_EXIST=$(az container show -g $RESOURCE_GROUP -n $ACI_NAME &>/dev/null; echo $?)
-    if [ $ACI_EXIST -ne 0 ]
+    ACR_EXIST=$(az acr show -g $RESOURCE_GROUP -n $ACI_NAME &>/dev/null; echo $?)
+    if [ $ACR_EXIST -ne 0 ]
     then
-        echo -e "\n--> ERROR: Failed to create container instance $ACI_NAME in resource group $RESOURCE_GROUP ...\n"
+        echo -e "\n--> ERROR: Failed to create Container Registry $ACR_NAME in resource group $RESOURCE_GROUP ...\n"
         exit 5
     fi
 }
 
-# Usage text
+## Usage text
 function print_usage_text () {
-    NAME_EXEC="aci-flp-labs"
+    NAME_EXEC="acr-flp-labs"
     echo -e "$NAME_EXEC usage: $NAME_EXEC -l <LAB#> -u <USER_ALIAS> [-v|--validate] [-r|--region] [-h|--help] [--version]\n"
     echo -e "\nHere is the list of current labs available:\n
 *************************************************************************************
 CORE LABS:
-*\t 1. ACI deployment failure configuration
-*\t 2. ACI deployment authorization failed
-*\t 3. ACI connection issue between 2 container groups V1
-*\t 4. ACI deployment failed netwwork configuration V1
-*\t 5. ACI deployment failed with Log analytics
-*\t 6. ACI container create failure with Azure File mount
-*\t 7. ACI deployment failure with Storage account
-*\t 8. ACI container create image pull failure V1
+*\t 1. ACR Network - LAB 01
+*\t 2. ACR Network - LAB 02
 
-
-EXTRA LABS:
-*\t 9. ACI deployment failure on pre-existing vnet
-*\t 10. ACI container continuous restart issue
-*\t 11. ACI container create image pull failure V2
-*\t 12. ACI deployment failed netwwork configuration V2 
-*\t 13. ACI connection issue between 2 container groups V2
-*\t 14. ACI connection issue to container
 *************************************************************************************\n"
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
@@ -222,87 +224,9 @@ eploy the second container instance \"appcontaineryaml\"\n"
 
 
 
-# Lab scenario 2
-function lab_scenario_2 () {
-  ACI_SP_NAME="sp-aci-lab2"
-  ACI_NAME="mycontainer"
-  RESOURCE_GROUP=aci-labs-ex${LAB_SCENARIO}-rg-${USER_ALIAS}
-
-  check_resourcegroup_cluster $RESOURCE_GROUP $ACI_NAME
-
-  echo -e "\n--> Deploying resources for lab${LAB_SCENARIO}...\n"
-
-  ACI_RG_URI=$(az group list \
-   --output json | jq -r ".[] | select ( .name == \"$RESOURCE_GROUP\") | [ .id] | @tsv")
 
 
-  declare -a ARR_SP_DETAILS
-
-  ARR_SP_DETAILS=($(az ad sp create-for-rbac \
-  --name $ACI_SP_NAME \
-  --role Reader \
-  --scopes $ACI_RG_URI 2>/dev/null | jq -r ". | [ .password, .appId , .displayName ] | @tsv"))
-
-  TENANT=$(az account list \
-    --output json | jq -r ".[] | select ( .isDefault == "true" ) | [ .tenantId] | @tsv")
-
-  #AZ_LOGIN_STRING=$(echo "az login --service-principal --username ${ARR_SP_DETAILS[1]} --password ${ARR_SP_DETAILS[0]} --tenant $TENANT") 
-  #"Login With Another SP"
-  #bash $AZ_LOGIN_STRING &>/dev/null
-
-  #echo "Waiting... 60s"
-  sleep 60
-  
-  # Do the SP Login
-  az login --service-principal --username ${ARR_SP_DETAILS[1]} --password ${ARR_SP_DETAILS[0]} --tenant $TENANT  &>/dev/null
-
-  ## Create Container
-  ERROR_MESSAGE="$(az container create \
-    --resource-group $RESOURCE_GROUP \
-    --name $ACI_NAME \
-    --image mcr.microsoft.com/azure-cli \
-    --command-line "sleep infinity" 2>&1)"  
-
-  echo -e "\n\n************************************************************************\n"
-  echo -e "\n--> Issue description: \n Customer needs to deploy an ACI in the resource group $RESOURCE_GROUP"
-  echo -e "az container create --resource-group $RESOURCE_GROUP --name $ACI_NAME --image mcr.microsoft.com/azure-cli --command-line \"tail -f /dev/null\"\n"
-  echo -e "Cx is getting the error message:"
-  echo -e "\n-------------------------------------------------------------------------------------\n"
-  echo -e "$ERROR_MESSAGE"
-  echo -e "\n-------------------------------------------------------------------------------------\n"
-  echo -e "Once you find the issue, run again the previous command to deploy ACI"
-
-}
-
-
-function lab_scenario_2_validation () {
-  ACI_SP_NAME="sp-aci-lab2"
-  ACI_NAME="mycontainer"
-  RESOURCE_GROUP=aci-labs-ex${LAB_SCENARIO}-rg-${USER_ALIAS}
-
-  ## Test se ACI corre com SP criado
-  ACI_STATUS=$(az container list \
-    --output json  | jq -r ".[] | select ( .name == \"$ACI_NAME\" ) | select ( .resourceGroup == \"$RESOURCE_GROUP\") | [ .id] | @tsv" | wc -l) 
-
-
-  if [[ "$ACI_STATUS" == "1" ]]
-  then
-        echo -e "\n\n========================================================"
-        echo -e "\nContainer instance $ACI_NAME looks good now!\n"
-  else
-        echo -e "\n--> Error: Scenario $LAB_SCENARIO is still FAILED\n\n"
-        echo -e "Once you find the issue, run agains the $LAB_SCENARIO"
-  fi
-
-}
-
-
-
-
-
-
-
-#if -h | --help option is selected usage will be displayed
+## If -h | --help option is selected usage will be displayed
 if [ $HELP -eq 1 ]
 then
 	print_usage_text
@@ -333,16 +257,20 @@ fi
 
 # lab scenario has a valid option
 
-REG_EX="^\\b([1-9]|1[0-4])\\b"
+REG_EX="^\\b([1-9]|)\\b"
 
 if [[ ! $LAB_SCENARIO =~ $REG_EX ]];
 then
-    echo -e "\n--> Error: invalid value for lab scenario '-l $LAB_SCENARIO'\nIt must be value from 1 to 14\n"
+    echo -e "\n--> Error: invalid value for lab scenario '-l $LAB_SCENARIO'\nIt must be value from 1 to 2\n"
     exit 11
 fi
 
-# main
-echo -e "\n--> ACI Troubleshooting sessions
+
+##########
+## main ##
+##########
+
+echo -e "\n--> ACR Troubleshooting Sessions
 ********************************************
 
 This tool will use your default subscription to deploy the lab environments.
@@ -365,90 +293,6 @@ then
 elif [ $LAB_SCENARIO -eq 2 ] && [ $VALIDATE -eq 1 ]
 then
     lab_scenario_2_validation
-elif [ $LAB_SCENARIO -eq 3 ] && [ $VALIDATE -eq 0 ]
-then
-    check_resourcegroup_cluster
-    lab_scenario_3
-elif [ $LAB_SCENARIO -eq 3 ] && [ $VALIDATE -eq 1 ]
-then
-    lab_scenario_3_validation
-elif [ $LAB_SCENARIO -eq 4 ] && [ $VALIDATE -eq 0 ]
-then
-    check_resourcegroup_cluster
-    lab_scenario_4
-elif [ $LAB_SCENARIO -eq 4 ] && [ $VALIDATE -eq 1 ]
-then
-    lab_scenario_4_validation
-elif [ $LAB_SCENARIO -eq 5 ] && [ $VALIDATE -eq 0 ] 
-then
-    check_resourcegroup_cluster
-    lab_scenario_5
-elif [ $LAB_SCENARIO -eq 5 ] && [ $VALIDATE -eq 1 ] 
-then
-    lab_scenario_5_validation
-elif [ $LAB_SCENARIO -eq 6 ] && [ $VALIDATE -eq 0 ] 
-then
-    check_resourcegroup_cluster
-    lab_scenario_6
-elif [ $LAB_SCENARIO -eq 6 ] && [ $VALIDATE -eq 1 ] 
-then
-    lab_scenario_6_validation
-elif [ $LAB_SCENARIO -eq 7 ] && [ $VALIDATE -eq 0 ]
-then
-    check_resourcegroup_cluster
-    lab_scenario_7
-elif [ $LAB_SCENARIO -eq 7 ] && [ $VALIDATE -eq 1 ]
-then
-    lab_scenario_7_validation
-elif [ $LAB_SCENARIO -eq 8 ] && [ $VALIDATE -eq 0 ]
-then
-    check_resourcegroup_cluster
-    lab_scenario_8
-elif [ $LAB_SCENARIO -eq 8 ] && [ $VALIDATE -eq 1 ]
-then
-    lab_scenario_8_validation
-elif [ $LAB_SCENARIO -eq 9 ] && [ $VALIDATE -eq 0 ]
-then
-    check_resourcegroup_cluster
-    lab_scenario_9
-elif [ $LAB_SCENARIO -eq 9 ] && [ $VALIDATE -eq 1 ]
-then
-    lab_scenario_9_validation
-elif [ $LAB_SCENARIO -eq 10 ] && [ $VALIDATE -eq 0 ]
-then
-    check_resourcegroup_cluster
-    lab_scenario_10
-elif [ $LAB_SCENARIO -eq 10 ] && [ $VALIDATE -eq 1 ]
-then
-    lab_scenario_7_validation
-elif [ $LAB_SCENARIO -eq 11 ] && [ $VALIDATE -eq 0 ]
-then
-    check_resourcegroup_cluster
-    lab_scenario_11
-elif [ $LAB_SCENARIO -eq 11 ] && [ $VALIDATE -eq 1 ]
-then
-    lab_scenario_11_validation
-elif [ $LAB_SCENARIO -eq 12 ] && [ $VALIDATE -eq 0 ]
-then
-    check_resourcegroup_cluster
-    lab_scenario_12
-elif [ $LAB_SCENARIO -eq 12 ] && [ $VALIDATE -eq 1 ]
-then
-    lab_scenario_12_validation
-elif [ $LAB_SCENARIO -eq 13 ] && [ $VALIDATE -eq 0 ]
-then
-    check_resourcegroup_cluster
-    lab_scenario_13
-elif [ $LAB_SCENARIO -eq 13 ] && [ $VALIDATE -eq 1 ]
-then
-    lab_scenario_13_validation
-elif [ $LAB_SCENARIO -eq 14 ] && [ $VALIDATE -eq 0 ]
-then
-    check_resourcegroup_cluster
-    lab_scenario_14
-elif [ $LAB_SCENARIO -eq 14 ] && [ $VALIDATE -eq 1 ]
-then
-    lab_scenario_14_validation
 else
     echo -e "\n--> Error: no valid option provided\n"
     exit 12
